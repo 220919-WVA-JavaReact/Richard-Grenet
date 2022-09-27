32843fd105a98153e33b3664b5ed48c2f9f384f2
Eokn <EoknDev@gmail.com>
2022-09-27 14:44:28 -0400

    adding sql script assignment

---
+++ assignments/week 2/927/Countries.sql
@@ -0,0 +1,13 @@
+CREATE TABLE countries (
+	name varchar(30),
+	continent varchar(20),
+	capital varchar(20),
+	populationM int,
+	partofUN boolean
+);
+
+INSERT INTO countries VALUES ('United States of America', 'North America', 'Washington D.C.', 330, true);
+INSERT INTO countries VALUES ('France', 'Europe', 'Paris', 67, true);
+INSERT INTO countries VALUES ('Venezuela', 'Caracas', 'Paris', 28, true);
+INSERT INTO countries VALUES ('Afghanistan', 'Asia', 'Kabul', 39, true);
+INSERT INTO countries VALUES ('Panama', 'North America', 'Panama City', 4, true);
