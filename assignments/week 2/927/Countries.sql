36402ae56d912f40e58922eff6bcc43eb064aa38
Eokn <EoknDev@gmail.com>
2022-09-27 14:57:56 -0400

    fixed typo

--- assignments/week 2/927/Countries.sql
+++ assignments/week 2/927/Countries.sql
@@ -8,6 +8,6 @@ CREATE TABLE countries (
 
 INSERT INTO countries VALUES ('United States of America', 'North America', 'Washington D.C.', 330, true);
 INSERT INTO countries VALUES ('France', 'Europe', 'Paris', 67, true);
-INSERT INTO countries VALUES ('Venezuela', 'Caracas', 'Paris', 28, true);
+INSERT INTO countries VALUES ('Venezuela', 'South America', 'Caracas', 28, true);
 INSERT INTO countries VALUES ('Afghanistan', 'Asia', 'Kabul', 39, true);
 INSERT INTO countries VALUES ('Panama', 'North America', 'Panama City', 4, true);
